d8cb1929a0a7d3c0f910721453a5a648f07660b3
Madhumitha <madhumithavenu03@gmail.com>
2023-11-14 11:53:11 +0530

    looping and rendering news component

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -2,24 +2,47 @@ import React, { Component } from 'react'
 import NewsItem from '../newsitem/NewsItem'
 
 export class News extends Component {
-  render() {
-    return (
-      <div className='container my-3'>
-        <h2>NewsMonkey - Top Headlines</h2>
-        <div className='row'>
-            <div className='col-md-4'>
-                <NewsItem title="my title" description="abcd" />
+
+    constructor() {
+        super();
+        this.state = {
+            articles: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(
+            "https://newsapi.org/v2/everything?q=tesla&from=2023-10-14&sortBy=publishedAt&apiKey=bd002a1cf4f64969a8819fcbfe3a190f"
+        ).then(res => res.json()).then(data => {
+            this.setState({ articles: data.articles });
+        });
+    }
+
+    render() {
+        console.log(this.state.articles);
+        return (
+            <div className='container my-3'>
+                <h2>NewsMonkey - Top Headlines</h2>
+                {(this.state.articles.length != 0) &&
+                    <div className='row'>
+                        {(this.state.articles.map((item, index) => {
+                            return (<div className='col-md-4' key={index}>
+                                <NewsItem
+                                    title={item.title}
+                                    description={item.description}
+                                    imageUrl={item.urlToImage}
+                                />
+                            </div>)
+            
+                }))
+                        }
+
+
+                    </div>
+                }
             </div>
-            <div className='col-md-4'>
-                <NewsItem title="my title" description="abcd" />
-            </div>
-            <div className='col-md-4'>
-                <NewsItem title="my title" description="abcd" />
-            </div>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default News
