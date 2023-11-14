49b4647e76bbddeb1087451e209d9586201a5813
Madhumitha <madhumithavenu03@gmail.com>
2023-11-14 11:33:07 +0530

    writing code inside newsitem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import News from './components/news/News';
 
 export class App extends Component {
   render() {
     return (
+      <div>
       <Navbar />
+      <News />
+      </div>
     )
   }
 }
