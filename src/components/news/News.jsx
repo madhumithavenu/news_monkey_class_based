51b11e7871ad7033e1e9f5a9bf97301b5dad410a
Madhumitha <madhumithavenu03@gmail.com>
2023-11-14 12:40:55 +0530

    Alerting state

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -6,15 +6,25 @@ export class News extends Component {
     constructor() {
         super();
         this.state = {
-            articles: []
+            articles: [],
+            page: 1,
+            loading: true,
+            totalArticles: 0
         }
+        this.handleNextClick = this.handleNextClick.bind(this);
+        this.handlePreviousClick = this.handlePreviousClick.bind(this);
     }
 
     componentDidMount() {
         fetch(
             "https://newsapi.org/v2/everything?q=tesla&from=2023-10-14&sortBy=publishedAt&apiKey=bd002a1cf4f64969a8819fcbfe3a190f"
         ).then(res => res.json()).then(data => {
-            this.setState({ articles: data.articles });
+            this.setState({ 
+                ...this.state,
+                articles: data.articles,
+                totalArticles: data.totalResults,
+                loading: false
+            });
         });
     }
 
