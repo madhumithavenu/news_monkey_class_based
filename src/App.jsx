f8cf8b339426e5eb26914e81066db9a65f1bd19e
Madhumitha <madhumithavenu03@gmail.com>
2023-11-14 11:10:45 +0530

    creating navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 export class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <Navbar />
     )
   }
 }
