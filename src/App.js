55593f51940d0fb7541d92363ec25ec8147a0406
Madhumitha <madhumithavenu03@gmail.com>
2023-11-14 10:51:43 +0530

    Writing the class function to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>Hello</h1>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
 
-export default App;
+export default App
